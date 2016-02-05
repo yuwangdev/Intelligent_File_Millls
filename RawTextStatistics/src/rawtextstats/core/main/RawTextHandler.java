3112c770c7dd0f56191ee10995fbd62dce2d333b
Yu Wang <yuwang@wpa076017.wireless.mcgill.ca>
2016-02-04 20:02:31 -0500

    finish handler and begin pdfhander

--- RawTextStatistics/src/rawtextstats/core/main/RawTextHandler.java
+++ RawTextStatistics/src/rawtextstats/core/main/RawTextHandler.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package rawtextstats.core.main;
 
@@ -8,103 +8,106 @@ import java.util.HashMap;
 
 /**
  * @author ywang
- *
  */
 public class RawTextHandler {
-	/*
+    /*
 	 * mode: single mode: multiple mode: config
 	 */
 
-	private String mode;
-	private String configFileLocation;
+    private String mode;
+    private String configFileLocation;
 
-	private String singleFileLocation;
-	private ArrayList<String> multipleFilesLocations;
-	ArrayList<ResultPerFile> result = new ArrayList<>();
+    private String singleFileLocation;
+    private ArrayList<String> multipleFilesLocations;
+    ArrayList<ResultPerFile> result = new ArrayList<>();
 
-	public RawTextHandler(String singleFileLocation) {
-		super();
-		this.singleFileLocation = singleFileLocation;
-		this.mode = "single";
-	}
+    public RawTextHandler(String singleFileLocation) {
+        super();
+        this.singleFileLocation = singleFileLocation;
+        this.mode = "single";
+    }
 
-	public RawTextHandler(String mode, ArrayList<String> multipleFilesLocations) throws IllegalAccessException {
-		super();
-		this.mode = mode;
-		this.multipleFilesLocations = multipleFilesLocations;
-		if (!this.mode.equals("multiple")) {
-			throw new IllegalAccessException("Multiple file uploading mode needs the mode of 'multiple'");
-		}
-	}
+    public RawTextHandler(String mode, ArrayList<String> multipleFilesLocations) throws IllegalAccessException {
+        super();
+        this.mode = mode;
+        this.multipleFilesLocations = multipleFilesLocations;
+        if (!this.mode.equals("multiple")) {
+            throw new IllegalAccessException("Multiple file uploading mode needs the mode of 'multiple'");
+        }
+    }
 
-	public RawTextHandler(String mode, String configFileLocation) throws IllegalAccessException {
-		super();
-		this.mode = mode;
-		this.configFileLocation = configFileLocation;
-		if (!this.mode.equals("config")) {
-			throw new IllegalAccessException("Multiple file uploading mode needs the mode of 'config'");
-		}
-	}
+    public RawTextHandler(String mode, String configFileLocation) throws IllegalAccessException {
+        super();
+        this.mode = mode;
+        this.configFileLocation = configFileLocation;
+        if (!this.mode.equals("config")) {
+            throw new IllegalAccessException("Multiple file uploading mode needs the mode of 'config'");
+        }
+    }
 
-	public ArrayList<ResultPerFile> analyze() {
+    public ArrayList<ResultPerFile> analyze() {
 
-		if (this.mode.equals("single")) {
-			result.add((new RawTextStats(this.singleFileLocation)).getResult());
+        if (this.mode.equals("single")) {
+            result.add((new RawTextStats(this.singleFileLocation)).getResult());
 
-		}
+        }
 
-		if (this.mode.equals("multiple")) {
-			for (String file : this.multipleFilesLocations) {
-				result.add((new RawTextStats(file)).getResult());
-			}
+        if (this.mode.equals("multiple")) {
+            for (String file : this.multipleFilesLocations) {
+                result.add((new RawTextStats(file)).getResult());
+            }
 
-		}
+        }
 
-		if (this.mode.equals("config")) {
-			ArrayList<String> fileList = obtainFilesFromConfig(this.configFileLocation);
+        if (this.mode.equals("config")) {
+            ArrayList<String> fileList = obtainFilesFromConfig(this.configFileLocation);
 
-			for (String file : fileList) {
-				result.add((new RawTextStats(file)).getResult());
-			}
-		}
+            for (String file : fileList) {
+                result.add((new RawTextStats(file)).getResult());
+            }
+        }
 
-		return result;
+        return result;
 
-	}
-	
-	(new ResultPerFile(an.calFullContent(), an.calTotalLineNumber(), an.calWordNumberPerLine(),
-			an.calAverageWords(), an.calTotalWords(), an.calSymbol()));
+    }
 
-	
-	public HashMap<String, Integer> parseResult(){
-		HashMap<String, Integer> hm = new HashMap<>();
-		for (ResultPerFile rpf : result) {
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return hm;
-		
-	}
-	
-	
-	private ArrayList<String> obtainFilesFromConfig(String cfg) {
-		// TODO Auto-generated method stub
-		ArrayList<String> list = new ArrayList<>();
-		ReadRawFile rrf = new ReadRawFile(cfg);
-		list = rrf.convertFileToStringArray();
-		if (list.size() < 0) {
-			throw new IllegalAccessError("The configure file shoud not be empty");
-		}
 
-		return list;
-	}
+    public ArrayList<HashMap<String, Integer>> parseResult() {
+        ArrayList<HashMap<String, Integer>> r = new ArrayList<>();
+
+        HashMap<String, Integer> tmp = new HashMap<>();
+
+        for (ResultPerFile rpf : result) {
+            tmp.put("lineNumber", rpf.getTotalLineNumber());
+            tmp.put("averageWordNumber", rpf.getAverageWords());
+            tmp.put("totalWordNumber", rpf.getTotalWords());
+            tmp.put("letter", rpf.getSymbols().get("letter"));
+            tmp.put("digit", rpf.getSymbols().get("digit"));
+            tmp.put("blank", rpf.getSymbols().get("blank"));
+            tmp.put("comma", rpf.getSymbols().get("comma"));
+            tmp.put("exclamatory", rpf.getSymbols().get("exclamatory"));
+            tmp.put("question", rpf.getSymbols().get("question"));
+            tmp.put("period", rpf.getSymbols().get("period"));
+            tmp.put("colon", rpf.getSymbols().get("colon"));
+            tmp.put("semicolon", rpf.getSymbols().get("semicolon"));
+
+
+        }
+
+        return r;
+    }
+
+
+    private ArrayList<String> obtainFilesFromConfig(String cfg) {
+        // TODO Auto-generated method stub
+        ArrayList<String> list = new ArrayList<>();
+        ReadRawFile rrf = new ReadRawFile(cfg);
+        list = rrf.convertFileToStringArray();
+        if (list.size() < 0) {
+            throw new IllegalAccessError("The configure file shoud not be empty");
+        }
+
+        return list;
+    }
 
 }
---
+++ RawTextStatistics/test/rawtextstats/core/main/RawTextHandlerTest.java
@@ -0,0 +1,20 @@
+package rawtextstats.core.main;
+
+import org.junit.Test;
+
+/**
+ * Created by yuwang on 2/4/16.
+ */
+public class RawTextHandlerTest {
+
+    @Test
+    public void testAnalyze() throws Exception {
+
+
+    }
+
+    @Test
+    public void testParseResult() throws Exception {
+
+    }
+}
